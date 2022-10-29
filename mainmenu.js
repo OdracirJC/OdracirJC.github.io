437241804607ddce1f86b945dbfc6f88044b0f88
Ricardo J. Cantarero <rjc360@scarletmail.rutgers.edu>
2022-10-29 18:10:16 -0400

    Changed Background

--- mainmenu.js
+++ mainmenu.js
@@ -1,21 +1,14 @@
-
-
-
 var gza = "GZA_4thChamber.mp3";
 let isGza = false;
 
-function liquidSwords(){
-  if(!isGza)
-    {
-      song.play();
-      isGza = true;
-    }else
-      {
-        song.pause();
-        isGza = false;
-      }
-
-
+function liquidSwords() {
+  if (!isGza) {
+    song.play();
+    isGza = true;
+  } else {
+    song.pause();
+    isGza = false;
+  }
 }
 var rubiksdiv = document.getElementById("mymusic");
 
