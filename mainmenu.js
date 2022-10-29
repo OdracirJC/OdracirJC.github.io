92ba19a3ec5b266a0d7d07550eaf0baf1573c9d8
Ricardo J. Cantarero <rjc360@scarletmail.rutgers.edu>
2022-10-29 17:50:43 -0400

    Copied my old summer project and put it into folder

---
+++ mainmenu.js
@@ -0,0 +1,24 @@
+
+
+
+var gza = "GZA_4thChamber.mp3";
+let isGza = false;
+
+function liquidSwords(){
+  if(!isGza)
+    {
+      song.play();
+      isGza = true;
+    }else
+      {
+        song.pause();
+        isGza = false;
+      }
+
+
+}
+var rubiksdiv = document.getElementById("mymusic");
+
+var song = document.getElementById("gza");
+
+rubiksdiv.addEventListener("click", liquidSwords);
